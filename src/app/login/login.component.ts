d8b1905a63117441bf26f02881798f577b1696a8
gaboroneszabo <puma678@gmail.com>
2018-07-16 11:43:52 +0200

    add basic login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  username;
+  password;
 
   ngOnInit() {
+  }
+
+  login() {
+    this.http.post("api/login", {username:this.username, password:this.password}).subscribe(console.log);
   }
 
 }
