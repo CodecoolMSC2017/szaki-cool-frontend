aa54b23bd6e9cacdc7f41180ad2be3f19e425234
dnorbert4000 <dnorbert@gmail.com>
2018-07-17 11:13:40 +0200

    email field added for registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,13 +11,14 @@ export class RegisterComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   username;
+  email;
   password;
 
   ngOnInit() {
   }
 
   register() {
-    this.http.post("api/register", {username:this.username, password:this.password}).subscribe(console.log);
+    this.http.post("api/register", {username:this.username, email:this.email, password:this.password}).subscribe(console.log);
   }
 
 }
