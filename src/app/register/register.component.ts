2fc3d592e76ae02e1094e2faf09bd9a5b79c8913
gaboroneszabo <puma678@gmail.com>
2018-07-17 11:47:48 +0200

    username check status

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,11 +13,24 @@ export class RegisterComponent implements OnInit {
   username;
   password;
 
+  isAvailable:boolean = true;
+
   ngOnInit() {
   }
 
   register() {
     this.http.post("api/register", {username:this.username, password:this.password}).subscribe(console.log);
+  }
+
+  testName() {
+    this.http.get('api/users/check/' + this.username).subscribe(resp => {
+      if (resp) {
+        this.isAvailable = false;
+      }
+      else {
+        this.isAvailable = true;
+      }
+    });
   }
 
 }
