94a839d107551cd5824bd9d0eaa5ad3220b6f79a
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-07-31 14:03:23 +0200

    asdview

--- src/app/adsview/adsview.component.ts
+++ src/app/adsview/adsview.component.ts
@@ -8,6 +8,9 @@ import { Route, Router } from '@angular/router';
 })
 export class AdsviewComponent implements OnInit {
 
+  smallPicUrl;
+  bigPicEl;
+
   constructor(
     private router: Router
   ) {}
@@ -16,6 +19,16 @@ export class AdsviewComponent implements OnInit {
     if (sessionStorage.getItem("user") == null) {
       this.router.navigate(["/login"]);
     }
+  }
+
+  changePic(imageNumber) {
+    this.smallPicUrl = document.getElementById("smallPic" + imageNumber).getAttribute("src");
+    this.bigPicEl = document.getElementById("bigPic");
+
+    this.bigPicEl.style = "background: url("+this.smallPicUrl+");" + 
+    "background-size: auto 100%;" + 
+    "background-position: center center;" + 
+    "background-repeat: no-repeat;";
   }
 
 }
