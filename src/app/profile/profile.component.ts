80bfbb2ff30b69ff8fb66c16e2bb2d7010c67d47
gaboroneszabo <puma678@gmail.com>
2018-08-02 10:44:11 +0200

    user profile input box fix

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,24 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject, pipe } from 'rxjs';
-import {Http, Response, RequestOptions, Headers} from '@angular/http';
 import { HttpClient } from '@angular/common/http';
-
-
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
   username: String;
   profile;
-  probe = "probe1";
-  //profile: String;
-  //userId: String;
-
-  //constructor() { }
-
   edit = false;
   selectedFile;
 
@@ -26,19 +17,14 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit() {
-    //this.loadProfilePage();
     this.getProfile();
-    this.getUserId();
   }
 
   public getUserId() {
-     
     let userString = sessionStorage.getItem("user");
     let userObject = JSON.parse(userString);
-    //console.log(userObject.username);
     this.username = userObject.username;
     let userId = JSON.stringify(userObject.id);
-    //let name = user.name; 
     return userId;
   }
 
@@ -60,7 +46,20 @@ export class ProfileComponent implements OnInit {
   }
 
   saveChanges() {
-    this.http.post("api/profile/update", this.profile).subscribe(console.log);
+    this.profile.user_id = this.getProfile();
+    this.http.post("api/profile/update", {
+      firstName: this.profile.firstName,
+      lastName: this.profile.lastName,
+      userId: this.getUserId(),
+      phone: this.profile.phone,
+      address: this.profile.address,
+      description: this.profile.description,
+      picture: this.profile.picture
+    }).subscribe(resp =>{
+          console.log(resp);
+          this.getProfile();
+        }
+      );
   }
 
   uploadPic() {
@@ -68,12 +67,11 @@ export class ProfileComponent implements OnInit {
     fd.append('file', this.selectedFile, this.selectedFile.name);
     this.http.post('api/file', fd).subscribe(console.log);
     this.profile.picture = this.selectedFile.name;
-    this.saveChanges();
+    //this.saveChanges();
   }
 
   onFileSelected(event) {
     this.selectedFile = event.target.files[0];
     console.log(this.selectedFile);
   }
-
 }
