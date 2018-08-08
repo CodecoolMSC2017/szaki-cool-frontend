f6bd01e8ee542931ad40a79f39d9f7c104448fa5
gaboroneszabo <puma678@gmail.com>
2018-08-08 09:55:17 +0200

    add websocketservice

---
+++ src/app/websocket.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import { Message } from './message';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class WebsocketService {
+
+  constructor(private http: HttpClient) { }
+
+  private serverUrl = 'api/socket';
+  private stompClient;
+
+  messages = [];
+
+  connect() {
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, connected => {
+      that.onConnect();
+    });
+  }
+
+  onConnect() {
+    this.stompClient.subscribe("/user/reply/", (message) => {
+      //that.messages.push(JSON.parse(message.body));
+      this.messegeReceived(message);
+    });
+  }
+
+  messegeReceived(message) {
+    let parsedMessage : Message = JSON.parse(message.body);
+    this.messages.push(parsedMessage);
+  }
+
+  sendMessage(message: Message) {
+    this.stompClient.send('/app/message' ,{}, JSON.stringify(message));
+  }
+
+  getMessages() {
+    return this.messages;
+  }
+}
