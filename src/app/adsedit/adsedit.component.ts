897ac8dec61aee4db5dcbb7d20315649faa44f63
gaboroneszabo <puma678@gmail.com>
2018-08-22 13:47:16 +0200

    error message

--- src/app/adsedit/adsedit.component.ts
+++ src/app/adsedit/adsedit.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AddAdvertisement } from '../addAdvertisement';
 import { HttpClient } from '@angular/common/http';
 import { Route, Router } from '@angular/router';
+import { AdsError } from './adserror';
+import { CheckboxRequiredValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-adsedit',
@@ -11,6 +13,7 @@ import { Route, Router } from '@angular/router';
 export class AdseditComponent implements OnInit {
 
   addAdvertisement: AddAdvertisement = new AddAdvertisement();
+  error : AdsError = new AdsError;
 
   constructor(
     private http: HttpClient,
@@ -37,6 +40,49 @@ export class AdseditComponent implements OnInit {
     }).subscribe(console.log);
   }
 
+  checkValidYear() {
+    let num = Number(this.addAdvertisement.guarantee_value);
+    if(!num && this.addAdvertisement.guarantee_value != "") {
+      this.error.garanteeLength = "Year must be a number!"
+    }
+    else {
+      this.error.garanteeLength = "";
+    }
+  }
 
+  checkValidPrice() {
+    let num = Number(this.addAdvertisement.price);
+    if(!num && this.addAdvertisement.price != "") {
+      this.error.price = "Price must be a number!"
+    }
+    else {
+      this.error.price = "";
+    }
+  }
+
+  checkTitleEmpty() {
+    if(this.addAdvertisement.workTitle === "") {
+      this.error.title = "Title must be set!"
+    }
+    else {
+      this.error.title = "";
+    }
+  }
+
+  checkDescriptionEmpty() {
+    if(this.addAdvertisement.workDescription === "") {
+      this.error.description = "Description must be set!"
+    }
+    else {
+      this.error.description = "";
+    }
+  }
+
+  checkError() {
+    this.checkValidYear();
+    this.checkValidPrice();
+    this.checkTitleEmpty();
+    this.checkDescriptionEmpty();
+  }
 
 }
