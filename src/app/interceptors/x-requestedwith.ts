18d67afdb92a072e99fcfcfc7f36b97a728b4321
gaboroneszabo <puma678@gmail.com>
2018-09-03 14:57:48 +0200

    redirect to login page if session expired

--- src/app/interceptors/x-requestedwith.ts
+++ src/app/interceptors/x-requestedwith.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class XRequestedWithInterceptor implements HttpInterceptor {
 
+  constructor(private router: Router) {
+
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const clonedReq = req.clone({ setHeaders: {'X-Requested-With': 'XMLHttpRequest'} });
-    return next.handle(clonedReq);
+    return next.handle(clonedReq).pipe(tap((event: HttpEvent<any>) => {}, (err: any) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status == 401) {
+          sessionStorage.clear();
+          this.router.navigate(['/login']);
+        }
+      }
+    }));
   }
 } 
